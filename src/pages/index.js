f60d85c4c9775c0c20407de7c7639f1d05ec7539
Thrisha <thrishakannan25@gmail.com>
2023-10-03 02:39:29 +0530

    prizes section completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Hero from "@/sections/Hero/Hero";
+import Prizes from "@/sections/Prizes/Prizes";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      <Hero />
+      {/*<Hero />*/}
+        <Prizes />
     </main>
   )
 }
