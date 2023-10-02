e8479d71c4a7c73c25b71dfec5353c08ba9f0fec
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-10-02 23:26:00 +0530

    setup

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+const Hero = () => {
+    return (
+        <Fragment>
+            <div >
+                <h1 className="text-3xl text-red-950">Hero</h1>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Hero;
