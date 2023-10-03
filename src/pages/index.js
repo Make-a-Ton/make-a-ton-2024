358bd2936024885a2652970fd367eeab109b8af1
Rohit T P <tprohit9@gmail.com>
2023-10-03 12:35:24 +0530

    faq done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,24 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import Hero from "@/sections/Hero/Hero";
 import Join from "@/sections/join/join";
 import About from '@/sections/About';
 import Statistics from '@/sections/Statistics';
 import Prizes from "@/sections/Prizes/Prizes";
 import Footer from "@/sections/Footer/Footer";
+import Faq from "@/sections/Faq";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <About />
-      <Statistics />
-      <Prizes />
-      <Join />
-      <Footer />
-    </main>
-  )
+    return (
+        <main>
+            <Hero/>
+            <About/>
+            <Statistics/>
+            <Prizes/>
+            <Faq/>
+            <Join/>
+            <Footer/>
+        </main>
+    )
 }
