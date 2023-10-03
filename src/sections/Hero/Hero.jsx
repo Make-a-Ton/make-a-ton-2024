bbac50a71fc1c8bd44014660ce5205ceed5277ad
Rohit T P <tprohit9@gmail.com>
2023-10-04 04:13:43 +0530

    for the sunith

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Nav from "@/sections/Hero/Nav";
 import Countdown from "@/sections/Hero/Countdown";
 import Mobilenav from "@/sections/Hero/Mobilenav";
+import Link from "next/link";
 
 const Hero = () => {
 
@@ -16,7 +17,19 @@ const Hero = () => {
 
     return (
         <Fragment>
-            <div className="bg-[#204289] min-h-screen p-2">
+            <div className="star-container bg-[#204289] min-h-screen p-2">
+                <div className="line" />
+                <div className="line hidden lg:block" />
+                <div className="line hidden lg:block" />
+                <div className="line" />
+                <div className="line " />
+
+                <div className="line rotated" />
+                <div className="line rotated" />
+                <div className="line rotated" />
+                <div className="line rotated" />
+                <div className="line rotated" />
+
                 <div className="p-5 py-10">
                     {isMobile ? <Mobilenav/> : <Nav/>}
                 </div>
@@ -25,8 +38,8 @@ const Hero = () => {
                    <Image src={"/assets/logo.svg"} alt={"logo"} width={200} height={200}/>
                     <h1 className="hidden lg:block font-clash-bold text-5xl leading-10 py-10">Make-A-Ton<sup className="font-satoshi-regular">6.0</sup></h1>
                     <p className="text-center my-5 lg:my-1">A 24 HOUR HACKATHON TO EMBRACE THE SPIRIT OF INNOVATION</p>
-                    <button className={"animate-shake mt-3 bg-white text-black font-satoshi-bold rounded-[15px] px-5 py-3 border-8 border-gray-500"}>
-                        register</button>
+                    <Link href="https://forms.eventsradar.in/makeaton/" className={"animate-shake mt-3 bg-white text-black font-satoshi-bold rounded-[15px] px-5 py-3 border-8 border-gray-500"}>
+                        register</Link>
                     <p className="mt-10">4th - 5th November, 2023</p>
                     <div className="flex flex-row mt-5 justify-center items-center">
                         <Image src={"/assets/location.svg"} alt={"location"} width={20} height={20}/>
