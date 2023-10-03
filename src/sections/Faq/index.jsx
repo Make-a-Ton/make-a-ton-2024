71d5a5e4e28ef78f7734cf65847107a6229255fc
Rohit T P <tprohit9@gmail.com>
2023-10-04 03:23:49 +0530

    align

--- src/sections/Faq/index.jsx
+++ src/sections/Faq/index.jsx
@@ -5,30 +5,30 @@ import Accordion from "@/sections/Faq/Accordion";
 const Index = () => {
 
     return (
-      <section className='bg-[#204289] lg:p-20'>
-  <div className="flex flex-wrap">
-    {/* FAQ Heading on the left for desktop */}
-    <div className="w-6/12 lg:w-4/12 ">
-      <div className="h-full flex flex-col justify-center items-center">
-        <h1 className='text-white text-4xl font-bold leading-12 tracking-normal text-left'>
-          FAQ
-        </h1>
-      </div>
-    </div>
+        <section className='bg-[#204289] lg:p-20'>
+            <div className="flex flex-wrap justify-center items-center">
+                {/* FAQ Heading on the left for desktop */}
+                <div className="w-6/12 lg:w-4/12 ">
+                    <div className="h-full flex flex-col justify-center items-center">
+                        <h1 className='text-white text-4xl font-bold leading-12 tracking-normal text-left my-5'>
+                            FAQ
+                        </h1>
+                    </div>
+                </div>
 
-    {/* FAQs section */}
-    <div className="w-full lg:w-7/12">
-      <div
-        id="accordion-flush"
-        className="bg-[#204289] max-h-[500px] overflow-y-auto pr-8"
-      >
-        {FAQS.map((faq, index) => (
-          <Accordion key={index} question={faq.ques} answer={faq.ans} />
-        ))}
-      </div>
-    </div>
-  </div>
-</section>
+                {/* FAQs section */}
+                <div className="w-full lg:w-7/12 mx-5">
+                    <div
+                        id="accordion-flush"
+                        className="bg-[#204289] lg:max-h-[500px] overflow-y-auto lg:pr-8"
+                    >
+                        {FAQS.map((faq, index) => (
+                            <Accordion key={index} question={faq.ques} answer={faq.ans}/>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
 
     );
 };
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -16,25 +16,26 @@ const Hero = () => {
 
     return (
         <Fragment>
-            <div className="bg-gray-900 min-h-screen p-2">
+            <div className="bg-[#204289] min-h-screen p-2">
                 <div className="p-5 py-10">
                     {isMobile ? <Mobilenav/> : <Nav/>}
                 </div>
 
                 <div className="flex flex-col text-white font-satoshi-regular items-center justify-center">
                    <Image src={"/assets/logo.svg"} alt={"logo"} width={200} height={200}/>
-                    <h1 className="hidden lg:visible font-clash-bold text-5xl leading-10 py-10">Make-A-Ton<sup className="font-satoshi-regular">6.0</sup></h1>
-                    <p className="hidden lg:visible">A 24 HOUR HACKATHON TO EMBRACE THE SPIRIT OF INNOVATION</p>
-                    <button className={"animate-shake mt-3 bg-white text-black font-satoshi-bold rounded-[15px] px-5 py-3 border-8 border-gray-500 rotate-20 ease-in-out active:scale-90"}>
+                    <h1 className="hidden lg:block font-clash-bold text-5xl leading-10 py-10">Make-A-Ton<sup className="font-satoshi-regular">6.0</sup></h1>
+                    <p className="text-center my-5 lg:my-1">A 24 HOUR HACKATHON TO EMBRACE THE SPIRIT OF INNOVATION</p>
+                    <button className={"animate-shake mt-3 bg-white text-black font-satoshi-bold rounded-[15px] px-5 py-3 border-8 border-gray-500"}>
                         register</button>
                     <p className="mt-10">4th - 5th November, 2023</p>
                     <div className="flex flex-row mt-5 justify-center items-center">
                         <Image src={"/assets/location.svg"} alt={"location"} width={20} height={20}/>
-                        <p className="mx-3">Cochin University of Science and Technology</p>
+                        <p className="mx-3 hidden lg:block">Cochin University of Science and Technology</p>
+                        <p className="mx-3 block lg:hidden">CUSAT</p>
                     </div>
                 </div>
 
-                <div className="mt-10">
+                <div className="mt-10 hidden lg:block">
                         <Countdown/>
                 </div>
 
