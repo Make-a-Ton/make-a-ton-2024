f2c2bfd7c881196abe78d61edd213b2035fb4fb6
AjnasNB <ajnasnb@gmail.com>
2023-10-03 21:34:05 +0530

    updated schedule

---
+++ src/sections/schedule/schedule.js
@@ -0,0 +1,155 @@
+import React from 'react'
+import Box from './box-telegram'
+
+
+const schedule = () => {
+    return (
+        <div>
+
+            <div><Box /></div>
+            <div className="mb-10  sm:flex  sm:justify-center sm:items-center sm:flex-row sm:mb-10 sm:ml-0  sm:text-center text-center ml-[85px] justify-center items-center ">
+
+                <div className='relative bg-[#EAAF20]  h-[390px] w-[255px] p-12 flex justify-center items-center  '>
+                    <div className="
+                     text-[#204289] text-center font-clash-bold text-4xl leading-10    sm:rotate-[-90deg] rotate-0">
+                        Schedule
+                    </div>
+                </div>
+                <div className="top-1/2 sm:translate-y-1/2 sm:mt-0 mt-4 sm:translate-x-0 translate-x-1/4">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="41"
+                        height="6"
+                        viewBox="0 0 41 6"
+                        fill="none"
+                        className="transform sm:rotate-0 rotate-90 "
+                    >
+                        <path d="M0.399902 3H40.3999" stroke="#204289" stroke-width="5" />
+                    </svg>
+                </div>
+
+                <div />
+
+
+                <div className='relative border-2 border-[#204289] bg-[#F6F5F5]  h-[390px] flex flex-col justify-center items-center w-[255px] p-12 '>
+                    <div className='text-[#204289] font-satoshi-medium text-[25px] whitespace-nowrap '>
+                        Registration Starts
+
+                    </div>
+                    <div className='text-[#204289] font-satoshi-regular text-[22px] italic'>
+                        19th Sept
+                    </div>
+                    <div className='mt-2 whitespace-nowrap flex items-center'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="24" viewBox="0 0 26 24" fill="none">
+                            <path d="M13.021 22.6787C19.4527 22.6787 24.6665 17.8977 24.6665 12.0001C24.6665 6.10249 19.4527 1.32153 13.021 1.32153C6.58937 1.32153 1.37549 6.10249 1.37549 12.0001C1.37549 17.8977 6.58937 22.6787 13.021 22.6787Z" stroke="#204289" stroke-width="2.14286" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M13.0208 12.8216C13.5156 12.8216 13.9166 12.4538 13.9166 12.0001C13.9166 11.5465 13.5156 11.1787 13.0208 11.1787C12.5261 11.1787 12.125 11.5465 12.125 12.0001C12.125 12.4538 12.5261 12.8216 13.0208 12.8216Z" stroke="#204289" stroke-width="2.14286" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7.6463 12.8216C8.14104 12.8216 8.54211 12.4538 8.54211 12.0001C8.54211 11.5465 8.14104 11.1787 7.6463 11.1787C7.15156 11.1787 6.75049 11.5465 6.75049 12.0001C6.75049 12.4538 7.15156 12.8216 7.6463 12.8216Z" stroke="#204289" stroke-width="2.14286" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M18.3958 12.8216C18.8906 12.8216 19.2916 12.4538 19.2916 12.0001C19.2916 11.5465 18.8906 11.1787 18.3958 11.1787C17.9011 11.1787 17.5 11.5465 17.5 12.0001C17.5 12.4538 17.9011 12.8216 18.3958 12.8216Z" stroke="#204289" stroke-width="2.14286" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg><span className='ml-1 text-[#204289]'>Online</span>
+                    </div>
+                </div>
+                <div className="top-1/2 sm:translate-y-1/2 sm:mt-0 mt-4 sm:translate-x-0 translate-x-1/4">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="41"
+                        height="6"
+                        viewBox="0 0 41 6"
+                        fill="none"
+                        className="transform sm:rotate-0 rotate-90 "
+                    >
+                        <path d="M0.399902 3H40.3999" stroke="#204289" stroke-width="5" />
+                    </svg>
+                </div>
+                <div />
+
+
+                <div className='relative border-2 border-[#204289] bg-[#F6F5F5]  h-[390px] w-[255px] p-12 flex flex-col justify-center items-center'>
+                    <div className='text-[#204289] font-satoshi-medium text-[25px] whitespace-nowrap '>
+                        Registration Ends
+
+                    </div>
+                    <div className='text-[#204289] font-satoshi-regular text-[22px] italic'>
+                        5th Oct
+                        <br />
+                        1.49 PM
+
+                    </div>
+                    <div className='mt-2 whitespace-nowrap flex items-center'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="24" viewBox="0 0 26 24" fill="none">
+                            <path d="M13.021 22.6787C19.4527 22.6787 24.6665 17.8977 24.6665 12.0001C24.6665 6.10249 19.4527 1.32153 13.021 1.32153C6.58937 1.32153 1.37549 6.10249 1.37549 12.0001C1.37549 17.8977 6.58937 22.6787 13.021 22.6787Z" stroke="#204289" stroke-width="2.14286" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M13.0208 12.8216C13.5156 12.8216 13.9166 12.4538 13.9166 12.0001C13.9166 11.5465 13.5156 11.1787 13.0208 11.1787C12.5261 11.1787 12.125 11.5465 12.125 12.0001C12.125 12.4538 12.5261 12.8216 13.0208 12.8216Z" stroke="#204289" stroke-width="2.14286" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7.6463 12.8216C8.14104 12.8216 8.54211 12.4538 8.54211 12.0001C8.54211 11.5465 8.14104 11.1787 7.6463 11.1787C7.15156 11.1787 6.75049 11.5465 6.75049 12.0001C6.75049 12.4538 7.15156 12.8216 7.6463 12.8216Z" stroke="#204289" stroke-width="2.14286" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M18.3958 12.8216C18.8906 12.8216 19.2916 12.4538 19.2916 12.0001C19.2916 11.5465 18.8906 11.1787 18.3958 11.1787C17.9011 11.1787 17.5 11.5465 17.5 12.0001C17.5 12.4538 17.9011 12.8216 18.3958 12.8216Z" stroke="#204289" stroke-width="2.14286" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg><span className='ml-1 text-[#204289]'>Online</span>
+                    </div>
+                </div>
+                <div className="top-1/2 sm:translate-y-1/2 sm:mt-0 mt-4 sm:translate-x-0 translate-x-1/4">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="41"
+                        height="6"
+                        viewBox="0 0 41 6"
+                        fill="none"
+                        className="transform sm:rotate-0 rotate-90 "
+                    >
+                        <path d="M0.399902 3H40.3999" stroke="#204289" stroke-width="5" />
+                    </svg>
+                </div>
+                <div />
+
+                <div className='relative bg-[#204289]  h-[390px] w-[255px] p-12 flex flex-col justify-center items-center'>
+                    <div className='text-[#ffffff] font-satoshi-regular text-[22px] text-center  '>
+                        Reporting at the Venue
+                        <br />
+                        4th Nov
+                        <br />
+                        8.30 - 9.30 AM
+
+                    </div>
+                    <div className='mt-2 whitespace-nowrap flex items-center '>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="27" viewBox="0 0 21 27" fill="none">
+                            <path d="M10.5477 6.075C11.5291 6.075 12.4702 6.43058 13.1641 7.06351C13.858 7.69645 14.2478 8.55489 14.2478 9.45C14.2478 9.89321 14.1521 10.3321 13.9662 10.7416C13.7802 11.151 13.5077 11.5231 13.1641 11.8365C12.8205 12.1499 12.4126 12.3985 11.9637 12.5681C11.5148 12.7377 11.0336 12.825 10.5477 12.825C9.56641 12.825 8.62528 12.4694 7.93138 11.8365C7.23748 11.2036 6.84765 10.3451 6.84765 9.45C6.84765 8.55489 7.23748 7.69645 7.93138 7.06351C8.62528 6.43058 9.56641 6.075 10.5477 6.075ZM10.5477 0C13.2954 0 15.9306 0.995622 17.8735 2.76784C19.8165 4.54006 20.908 6.9437 20.908 9.45C20.908 16.5375 10.5477 27 10.5477 27C10.5477 27 0.1875 16.5375 0.1875 9.45C0.1875 6.9437 1.27902 4.54006 3.22194 2.76784C5.16486 0.995622 7.80003 0 10.5477 0ZM10.5477 2.7C8.58509 2.7 6.70283 3.41116 5.31503 4.67703C3.92723 5.9429 3.14757 7.65979 3.14757 9.45C3.14757 10.8 3.14757 13.5 10.5477 22.5585C17.9479 13.5 17.9479 10.8 17.9479 9.45C17.9479 7.65979 17.1682 5.9429 15.7804 4.67703C14.3926 3.41116 12.5104 2.7 10.5477 2.7Z" fill="white" />
+                        </svg>
+                        <span className='ml-1 text-[#ffffff]'>CUSAT</span>
+                    </div>
+                </div>
+                <div className="top-1/2 sm:translate-y-1/2 sm:mt-0 mt-4 sm:translate-x-0 translate-x-1/4">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="41"
+                        height="6"
+                        viewBox="0 0 41 6"
+                        fill="none"
+                        className="transform sm:rotate-0 rotate-90 "
+                    >
+                        <path d="M0.399902 3H40.3999" stroke="#204289" stroke-width="5" />
+                    </svg>
+                </div>
+                <div />
+
+
+                <div className='relative bg-[#204289]  h-[390px] w-[255px] p-12 flex flex-col justify-center items-center whitespace-nowrap'>
+
+                    <div className='text-[#ffffff] font-satoshi-regular text-[28px] text-center  '>
+                        Hackathon Ends
+                        <br />
+                        5th Nov
+
+
+                    </div>
+                    <div className='mt-2 whitespace-nowrap flex items-center '>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="27" viewBox="0 0 21 27" fill="none">
+                            <path d="M10.5477 6.075C11.5291 6.075 12.4702 6.43058 13.1641 7.06351C13.858 7.69645 14.2478 8.55489 14.2478 9.45C14.2478 9.89321 14.1521 10.3321 13.9662 10.7416C13.7802 11.151 13.5077 11.5231 13.1641 11.8365C12.8205 12.1499 12.4126 12.3985 11.9637 12.5681C11.5148 12.7377 11.0336 12.825 10.5477 12.825C9.56641 12.825 8.62528 12.4694 7.93138 11.8365C7.23748 11.2036 6.84765 10.3451 6.84765 9.45C6.84765 8.55489 7.23748 7.69645 7.93138 7.06351C8.62528 6.43058 9.56641 6.075 10.5477 6.075ZM10.5477 0C13.2954 0 15.9306 0.995622 17.8735 2.76784C19.8165 4.54006 20.908 6.9437 20.908 9.45C20.908 16.5375 10.5477 27 10.5477 27C10.5477 27 0.1875 16.5375 0.1875 9.45C0.1875 6.9437 1.27902 4.54006 3.22194 2.76784C5.16486 0.995622 7.80003 0 10.5477 0ZM10.5477 2.7C8.58509 2.7 6.70283 3.41116 5.31503 4.67703C3.92723 5.9429 3.14757 7.65979 3.14757 9.45C3.14757 10.8 3.14757 13.5 10.5477 22.5585C17.9479 13.5 17.9479 10.8 17.9479 9.45C17.9479 7.65979 17.1682 5.9429 15.7804 4.67703C14.3926 3.41116 12.5104 2.7 10.5477 2.7Z" fill="white" />
+                        </svg>
+                        <span className='ml-1 text-[#ffffff]'>CUSAT</span>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div >
+    )
+}
+
+export default schedule
