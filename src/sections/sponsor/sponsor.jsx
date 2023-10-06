71a51352a76c030281df90f62cc7f0cf292cc8e2
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-10-07 00:38:33 +0530

    added gold sponsor

--- src/sections/sponsor/sponsor.jsx
+++ src/sections/sponsor/sponsor.jsx
@@ -19,6 +19,10 @@ const HackathonImages = [
     "/assets/mlh.png",
 ]
 
+const GoldImages = [
+    "/assets/cusat_tech.png",
+]
+
 
 const sponsor = () => {
     return (
@@ -75,9 +79,8 @@ const sponsor = () => {
                 </div>
 
                 <div className="flex flex-col flex-grow items-center justify-center">
-                    <h3 className="block mb-1 text-center text-[#204289] font-satoshi-bold text-3xl ">Title Sponsor </h3>
-                    <Link href={"https://clusterdev.com/"} target={"_blank"} >
-
+                    <h3 className="block mb-1 text-center text-[#204289] font-satoshi-bold text-3xl ">Title Sponsor</h3>
+                    <Link href={"https://clusterdev.com/"} target={"_blank"}>
                         <Image height={240} width={240} src="/assets/clusterdev.png" alt="clusterdev"
                                className="px-5 lg:px-auto"/>
                     </Link>
@@ -86,7 +89,8 @@ const sponsor = () => {
 
                 <div className="flex flex-grow justify-end">
                     <div className='sm:flex flex-row hidden md:visible'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="229" height="229" viewBox="0 0 229 229" fill="none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="229" height="229" viewBox="0 0 229 229"
+                             fill="none">
                             <rect x="227.651" y="57.3015" width="57.0547" height="57.0547"
                                   transform="rotate(90 227.651 57.3015)" fill="#204289"/>
                             <rect x="171.248" y="0.246826" width="57.0547" height="57.0547"
@@ -104,7 +108,8 @@ const sponsor = () => {
                             <rect x="57.1382" y="114.356" width="57.0547" height="57.0547"
                                   transform="rotate(90 57.1382 114.356)" fill="#204289"/>
                         </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="229" height="229" viewBox="0 0 229 229" fill="none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="229" height="229" viewBox="0 0 229 229"
+                             fill="none">
                             <rect x="227.651" y="57.3015" width="57.0547" height="57.0547"
                                   transform="rotate(90 227.651 57.3015)" fill="#204289"/>
                             <rect x="171.248" y="0.246826" width="57.0547" height="57.0547"
@@ -125,6 +130,22 @@ const sponsor = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="flex flex-col sm:flex-row items-center justify-center w-full my-10">
+                <div className="flex justify-center sm:w-[30%] text-[#204289] font-satoshi-regular text-[30px]">
+                    Gold Sponsors
+                </div>
+
+                <div className="flex justify-center sm:justify-start flex-wrap w-[70%]">
+                    {GoldImages.map((image, i) => (
+                        <div key={i}
+                             className={`flex flex-grow justify-center min-h-[150px] p-10 ${i % 2 == 0 ? 'bg-[#F6F5F5]' : ''}`}>
+                            <Image src={image} width={100} height={100} className="object-contain"/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
 
             <div className="flex flex-col sm:flex-row items-center justify-center w-full my-10">
                 <div className="flex justify-center sm:w-[30%] text-[#204289] font-satoshi-regular text-[30px]">
