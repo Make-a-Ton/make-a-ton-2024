e5956c9cb8210bb47741b002ad66daf62a4a79b4
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-10-07 00:10:01 +0530

    make mobile responsive

--- src/sections/sponsor/sponsor.jsx
+++ src/sections/sponsor/sponsor.jsx
@@ -20,28 +20,28 @@ const sponsor = () => {
     return (
         <Fragment>
 
-            <div className="flex items-center justify-center w-full my-10">
-                <div className="flex justify-center w-[30%] text-[#204289] font-satoshi-regular text-[30px]">
+            <div className="flex flex-col sm:flex-row items-center justify-center w-full my-10">
+                <div className="flex justify-center sm:w-[30%] text-[#204289] font-satoshi-regular text-[30px]">
                     Silver Sponsors
                 </div>
 
-                <div className="flex justify-start flex-wrap w-[70%]">
+                <div className="flex justify-center sm:justify-start flex-wrap w-[70%]">
                     {SilverImages.map((image, i) => (
-                        <div key={i} className={`flex p-10 ${i % 2 == 0 ? 'bg-[#F6F5F5]' : ''}`}>
+                        <div key={i} className={`flex min-h-[150px] p-10 ${i % 2 == 0 ? 'bg-[#F6F5F5]' : ''}`}>
                             <Image src={image} width={100} height={100} className="object-contain"/>
                         </div>
                     ))}
                 </div>
             </div>
 
-            <div className="flex items-center justify-center w-full my-10">
-                <div className="flex justify-center w-[30%] text-[#204289] font-satoshi-regular text-[30px]">
+            <div className="flex flex-col sm:flex-row items-center justify-center w-full my-10">
+                <div className="flex justify-center sm:w-[30%] text-[#204289] font-satoshi-regular text-[30px]">
                     Community Partners
                 </div>
 
-                <div className="flex justify-start flex-wrap w-[70%]">
+                <div className="flex justify-center sm:justify-start flex-wrap w-[70%]">
                     {CommunityImages.map((image, i) => (
-                        <div key={i} className={`flex p-10 ${i % 2 != 0 ? 'bg-[#F6F5F5]' : ''}`}>
+                        <div key={i} className={`flex min-h-[150px] p-10 ${i % 2 != 0 ? 'bg-[#F6F5F5]' : ''}`}>
                             <Image src={image} width={100} height={100} className="object-contain"/>
                         </div>
                     ))}
