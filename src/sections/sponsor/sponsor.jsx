c9b79824202333b1fdf6162ac3e61db5bdd2051a
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-10-07 00:35:26 +0530

    added title sponsor

--- src/sections/sponsor/sponsor.jsx
+++ src/sections/sponsor/sponsor.jsx
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react'
 import Image from "next/image";
+import Link from "next/link";
 
 const SilverImages = [
     "/assets/foss.svg",
@@ -14,11 +15,116 @@ const CommunityImages = [
     "/assets/gdsc cusat.png",
 ]
 
+const HackathonImages = [
+    "/assets/mlh.png",
+]
 
 
 const sponsor = () => {
     return (
         <Fragment>
+
+            <div className='flex justify-center items-center'>
+                <div className='text-blue-900 font-clash-bold text-5xl mt-10 mb-10 justify-center items-center'>
+                    PARTNERS
+                </div>
+            </div>
+
+            <div className="flex justify-around items-center">
+                <div className="flex flex-grow">
+                    <div className='hidden md:visible sm:flex flex-row'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="229" height="229" viewBox="0 0 229 229"
+                             fill="none">
+                            <rect x="227.651" y="57.3015" width="57.0547" height="57.0547"
+                                  transform="rotate(90 227.651 57.3015)" fill="#204289"/>
+                            <rect x="171.248" y="0.246826" width="57.0547" height="57.0547"
+                                  transform="rotate(90 171.248 0.246826)" fill="#204289"/>
+                            <rect x="228.302" y="171.411" width="57.0547" height="57.0547"
+                                  transform="rotate(90 228.302 171.411)" fill="#204289"/>
+                            <rect x="171.248" y="114.356" width="57.0547" height="57.0547"
+                                  transform="rotate(90 171.248 114.356)" fill="#204289"/>
+                            <rect x="114.193" y="171.411" width="57.0547" height="57.0547"
+                                  transform="rotate(90 114.193 171.411)" fill="#204289"/>
+                            <rect x="114.193" y="57.3015" width="57.0547" height="57.0547"
+                                  transform="rotate(90 114.193 57.3015)" fill="#204289"/>
+                            <rect x="57.7891" y="0.246826" width="57.0547" height="57.0547"
+                                  transform="rotate(90 57.7891 0.246826)" fill="#204289"/>
+                            <rect x="57.1382" y="114.356" width="57.0547" height="57.0547"
+                                  transform="rotate(90 57.1382 114.356)" fill="#204289"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="229" height="229" viewBox="0 0 229 229"
+                             fill="none">
+                            <rect x="227.651" y="57.3015" width="57.0547" height="57.0547"
+                                  transform="rotate(90 227.651 57.3015)" fill="#204289"/>
+                            <rect x="171.248" y="0.246826" width="57.0547" height="57.0547"
+                                  transform="rotate(90 171.248 0.246826)" fill="#204289"/>
+                            <rect x="228.302" y="171.411" width="57.0547" height="57.0547"
+                                  transform="rotate(90 228.302 171.411)" fill="#204289"/>
+                            <rect x="171.248" y="114.356" width="57.0547" height="57.0547"
+                                  transform="rotate(90 171.248 114.356)" fill="#204289"/>
+                            <rect x="114.193" y="171.411" width="57.0547" height="57.0547"
+                                  transform="rotate(90 114.193 171.411)" fill="#204289"/>
+                            <rect x="114.193" y="57.3015" width="57.0547" height="57.0547"
+                                  transform="rotate(90 114.193 57.3015)" fill="#204289"/>
+                            <rect x="57.7891" y="0.246826" width="57.0547" height="57.0547"
+                                  transform="rotate(90 57.7891 0.246826)" fill="#204289"/>
+                            <rect x="57.1382" y="114.356" width="57.0547" height="57.0547"
+                                  transform="rotate(90 57.1382 114.356)" fill="#204289"/>
+                        </svg>
+                    </div>
+                </div>
+
+                <div className="flex flex-col flex-grow items-center justify-center">
+                    <h3 className="block mb-1 text-center text-[#204289] font-satoshi-bold text-3xl ">Title Sponsor </h3>
+                    <Link href={"https://clusterdev.com/"} target={"_blank"} >
+
+                        <Image height={240} width={240} src="/assets/clusterdev.png" alt="clusterdev"
+                               className="px-5 lg:px-auto"/>
+                    </Link>
+
+                </div>
+
+                <div className="flex flex-grow justify-end">
+                    <div className='sm:flex flex-row hidden md:visible'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="229" height="229" viewBox="0 0 229 229" fill="none">
+                            <rect x="227.651" y="57.3015" width="57.0547" height="57.0547"
+                                  transform="rotate(90 227.651 57.3015)" fill="#204289"/>
+                            <rect x="171.248" y="0.246826" width="57.0547" height="57.0547"
+                                  transform="rotate(90 171.248 0.246826)" fill="#204289"/>
+                            <rect x="228.302" y="171.411" width="57.0547" height="57.0547"
+                                  transform="rotate(90 228.302 171.411)" fill="#204289"/>
+                            <rect x="171.248" y="114.356" width="57.0547" height="57.0547"
+                                  transform="rotate(90 171.248 114.356)" fill="#204289"/>
+                            <rect x="114.193" y="171.411" width="57.0547" height="57.0547"
+                                  transform="rotate(90 114.193 171.411)" fill="#204289"/>
+                            <rect x="114.193" y="57.3015" width="57.0547" height="57.0547"
+                                  transform="rotate(90 114.193 57.3015)" fill="#204289"/>
+                            <rect x="57.7891" y="0.246826" width="57.0547" height="57.0547"
+                                  transform="rotate(90 57.7891 0.246826)" fill="#204289"/>
+                            <rect x="57.1382" y="114.356" width="57.0547" height="57.0547"
+                                  transform="rotate(90 57.1382 114.356)" fill="#204289"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="229" height="229" viewBox="0 0 229 229" fill="none">
+                            <rect x="227.651" y="57.3015" width="57.0547" height="57.0547"
+                                  transform="rotate(90 227.651 57.3015)" fill="#204289"/>
+                            <rect x="171.248" y="0.246826" width="57.0547" height="57.0547"
+                                  transform="rotate(90 171.248 0.246826)" fill="#204289"/>
+                            <rect x="228.302" y="171.411" width="57.0547" height="57.0547"
+                                  transform="rotate(90 228.302 171.411)" fill="#204289"/>
+                            <rect x="171.248" y="114.356" width="57.0547" height="57.0547"
+                                  transform="rotate(90 171.248 114.356)" fill="#204289"/>
+                            <rect x="114.193" y="171.411" width="57.0547" height="57.0547"
+                                  transform="rotate(90 114.193 171.411)" fill="#204289"/>
+                            <rect x="114.193" y="57.3015" width="57.0547" height="57.0547"
+                                  transform="rotate(90 114.193 57.3015)" fill="#204289"/>
+                            <rect x="57.7891" y="0.246826" width="57.0547" height="57.0547"
+                                  transform="rotate(90 57.7891 0.246826)" fill="#204289"/>
+                            <rect x="57.1382" y="114.356" width="57.0547" height="57.0547"
+                                  transform="rotate(90 57.1382 114.356)" fill="#204289"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
 
             <div className="flex flex-col sm:flex-row items-center justify-center w-full my-10">
                 <div className="flex justify-center sm:w-[30%] text-[#204289] font-satoshi-regular text-[30px]">
@@ -27,7 +133,8 @@ const sponsor = () => {
 
                 <div className="flex justify-center sm:justify-start flex-wrap w-[70%]">
                     {SilverImages.map((image, i) => (
-                        <div key={i} className={`flex min-h-[150px] p-10 ${i % 2 == 0 ? 'bg-[#F6F5F5]' : ''}`}>
+                        <div key={i}
+                             className={`flex flex-grow justify-center min-h-[150px] p-10 ${i % 2 == 0 ? 'bg-[#F6F5F5]' : ''}`}>
                             <Image src={image} width={100} height={100} className="object-contain"/>
                         </div>
                     ))}
@@ -41,7 +148,23 @@ const sponsor = () => {
 
                 <div className="flex justify-center sm:justify-start flex-wrap w-[70%]">
                     {CommunityImages.map((image, i) => (
-                        <div key={i} className={`flex min-h-[150px] p-10 ${i % 2 != 0 ? 'bg-[#F6F5F5]' : ''}`}>
+                        <div key={i}
+                             className={`flex flex-grow justify-center  min-h-[150px] p-10 ${i % 2 != 0 ? 'bg-[#F6F5F5]' : ''}`}>
+                            <Image src={image} width={100} height={100} className="object-contain"/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="flex flex-col sm:flex-row items-center justify-center w-full my-10">
+                <div className="flex justify-center sm:w-[30%] text-[#204289] font-satoshi-regular text-[30px]">
+                    Hackathon Partner
+                </div>
+
+                <div className="flex justify-center sm:justify-start flex-wrap w-[70%]">
+                    {HackathonImages.map((image, i) => (
+                        <div key={i}
+                             className={`flex flex-grow justify-center  min-h-[150px] p-10 ${i % 2 != 0 ? 'bg-[#F6F5F5]' : ''}`}>
                             <Image src={image} width={100} height={100} className="object-contain"/>
                         </div>
                     ))}
