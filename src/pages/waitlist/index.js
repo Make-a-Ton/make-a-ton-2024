a82ec2eaf3b1c69cc3cdd3bd1297b110718382e2
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-10 21:31:27 +0530

    added form for waitlist

---
+++ src/pages/waitlist/index.js
@@ -0,0 +1,9 @@
+import {Frame} from "@/sections/airtable/Frame";
+
+export default function Waitlist() {
+    return (
+        <Frame iframe_link={"https://airtable.com/embed/appA5AR9d2RoZOIXY/shrlQwjlDeHxc7hP4?backgroundColor=blue"}
+        ></Frame>
+    )
+
+}
