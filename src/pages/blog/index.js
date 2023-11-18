42b40c7e407263c37ac80c3a78ab82409f0ffd90
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-11-18 10:44:18 +0530

    feat: '/blog' routed to Write-a-Byte

---
+++ src/pages/blog/index.js
@@ -0,0 +1,10 @@
+import {useEffect} from 'react'
+
+export default function Blog() {
+    useEffect(() => {
+        window.location.assign('https://medium.com/writeabyte/')
+    })
+    return(
+        <></>
+    )
+}
