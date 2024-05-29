9de2c9e8c89710c854bcdab3875319c7611f8753
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2024-05-29 22:29:00 +0530

    removed old join page

--- src/pages/join/ambience.js
+++
@@ -1,9 +0,0 @@
-import {Frame} from "@/sections/airtable/Frame";
-
-export default function Ambience() {
-    return (
-        <Frame iframe_link={"https://airtable.com/embed/appUIyS71gDsTNQmv/shrvf7PySeYpWrxH6?backgroundColor=blue"}
-        ></Frame>
-    )
-
-}
--- src/pages/join/media.js
+++
@@ -1,9 +0,0 @@
-import {Frame} from "@/sections/airtable/Frame";
-
-export default function Ambience() {
-    return (
-        <Frame iframe_link={"https://airtable.com/embed/appUIyS71gDsTNQmv/shr9NTjBt6JoOkVRs?backgroundColor=blue"}
-        ></Frame>
-    )
-
-}
