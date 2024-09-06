27a16cbf90f40e8f621b1693748e32323a25c8a2
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-09-07 04:58:59 +0530

    star-a-ton section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,57 @@
-import {Inter} from 'next/font/google'
+import { Inter } from "next/font/google";
 import Hero from "@/sections/Hero/Hero";
 import Join from "@/sections/join/join";
-import About from '@/sections/About';
-import Statistics from '@/sections/Statistics';
+import About from "@/sections/About";
+import Statistics from "@/sections/Statistics";
 import Prizes from "@/sections/Prizes/Prizes";
 import Footer from "@/sections/Footer/Footer";
 import Faq from "@/sections/Faq";
 import Schedule from "@/sections/schedule/schedule";
-import Sponsor from '@/sections/sponsor/sponsor';
+import Sponsor from "@/sections/sponsor/sponsor";
 import MlhPrizes from "@/sections/MlhPrizes/MlhPrizes";
 import Gallery from "@/sections/Gallery/Gallery";
+import StarATon from "@/sections/StarATon/StarATon";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  return (
+    <main>
+      <a
+        className="right-4 md:right-[80px]"
+        id="mlh-trust-badge"
+        style={{
+          display: "block",
+          maxWidth: "120px",
+          minWidth: "60px",
+          position: "fixed",
 
-    return (
-        <main>
-            <a
-                className="right-4 md:right-[80px]"
-                id="mlh-trust-badge"
-                style={{
-                    display: 'block',
-                    maxWidth: '120px',
-                    minWidth: '60px',
-                    position: 'fixed',
+          top: "0",
+          width: "10%",
+          zIndex: "10000",
+        }}
+        href="https://mlh.io/apac?utm_source=apac-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=yellow"
+        target="_blank"
+      >
+        <img
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-yellow.svg"
+          alt="Major League Hacking 2025 Hackathon Season"
+          style={{ width: "100%" }}
+        />
+      </a>
 
-                    top: '0',
-                    width: '10%',
-                    zIndex: '10000',
-                }}
-                href="https://mlh.io/apac?utm_source=apac-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=yellow"
-                target="_blank"
-            >
-                <img
-                    src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-yellow.svg"
-                    alt="Major League Hacking 2025 Hackathon Season"
-                    style={{width: '100%'}}
-                />
-            </a>
-        
-            <Hero/>
-            <About/>
-            <Statistics/>
-            <Prizes/>
-            {/* <MlhPrizes/> */}
-            {/* <Schedule/> */}
-            <Sponsor/>
-            <Faq/>
-            <Join/>
-            <Gallery/>
-            <Footer/>
-        </main>
-    )
+      <Hero />
+      <About />
+      <StarATon />
+      <Statistics />
+      <Prizes />
+      {/* <MlhPrizes/> */}
+      {/* <Schedule/> */}
+      <Sponsor />
+      <Faq />
+      <Join />
+      <Gallery />
+      <Footer />
+    </main>
+  );
 }
