90012a577e9cb0948754a9bf79e6df89149141d4
Karthik <karthiksujith789@gmail.com>
2024-09-07 01:22:38 +0530

    error fix

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -7,6 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
