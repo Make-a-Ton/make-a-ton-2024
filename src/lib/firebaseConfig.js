b36b921d86a6aaf6dea08b4bef35d39ab885fd33
Karthik <karthiksujith789@gmail.com>
2024-09-08 13:55:40 +0530

    final testing of env

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -10,12 +10,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "make-a-ton-32249.firebaseapp.com",
-  projectId: "make-a-ton-32249",
-  storageBucket: "make-a-ton-32249.appspot.com",
-  messagingSenderId: "619336380250",
-  appId: "1:619336380250:web:bc4075b9a925eddca50d63",
-  measurementId: "G-BWS52CBSDH",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
