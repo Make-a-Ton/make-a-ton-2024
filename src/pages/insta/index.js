50a094f2347859ca745721dfb69857e08024f02b
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2024-09-09 15:36:28 +0530

    Added instagram link

---
+++ src/pages/insta/index.js
@@ -0,0 +1,10 @@
+import {useEffect} from 'react'
+
+export default function Blog() {
+    useEffect(() => {
+        window.location.assign('https://instagram.com/makeaton.cusat')
+    })
+    return(
+        <></>
+    )
+}
