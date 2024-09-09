0fede68bb28301071772f0a8027fbf3e9285da76
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-09-10 00:01:57 +0530

    added rick roll

---
+++ src/pages/rr/index.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+export default function RickAstley() {
+  useEffect(() => {
+    window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  });
+  return <></>;
+}
