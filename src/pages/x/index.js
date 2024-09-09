130a85fe874887a4a21d23ebfce6ee93fb607193
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2024-09-09 15:39:31 +0530

    Added X link

---
+++ src/pages/x/index.js
@@ -0,0 +1,10 @@
+import {useEffect} from 'react'
+
+export default function X() {
+    useEffect(() => {
+        window.location.assign('https://www.twitter.com/MakeaTonCusat')
+    })
+    return(
+        <></>
+    )
+}
