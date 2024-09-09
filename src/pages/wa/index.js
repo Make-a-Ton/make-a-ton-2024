29f89d0a921b2d952abe20f95e08ff6690dd99ac
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2024-09-09 15:32:47 +0530

    Added whatsapp link as route

---
+++ src/pages/wa/index.js
@@ -0,0 +1,10 @@
+import {useEffect} from 'react'
+
+export default function Blog() {
+    useEffect(() => {
+        window.location.assign('https://whatsapp.com/channel/0029ValGCQM60eBjNF5wS10C')
+    })
+    return(
+        <></>
+    )
+}
