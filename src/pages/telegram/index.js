3a5f1ffda9377d461ffb6000cd8447cf0118543d
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2024-09-09 15:37:56 +0530

    Added telegram link

---
+++ src/pages/telegram/index.js
@@ -0,0 +1,10 @@
+import {useEffect} from 'react'
+
+export default function Telegram() {
+    useEffect(() => {
+        window.location.assign('https://t.me/makeaton7')
+    })
+    return(
+        <></>
+    )
+}
