255ceb0365b3522fa28db9597616759aaf38911f
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-09-14 22:56:03 +0530

    routing as before

---
+++ src/pages/linkedin/index.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+export default function X() {
+  useEffect(() => {
+    window.location.assign("https://www.linkedin.com/company/makeaton/posts/");
+  });
+  return <></>;
+}
---
+++ src/pages/rr/index.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+export default function X() {
+  useEffect(() => {
+    window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  });
+  return <></>;
+}
