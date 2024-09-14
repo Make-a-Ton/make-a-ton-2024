b72d128db895ea2954f65cd1adf4531c13d15d69
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-09-14 22:46:05 +0530

    slug moved [slug] directory

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,22 @@
-import {Head, Html, Main, NextScript} from 'next/document'
-import {Analytics} from "@vercel/analytics/react";
+import { Head, Html, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <link
-                rel="stylesheet"/>
-            <link rel="icon" type="image/x-icon" href="/assets/favicon.ico"/>
-            <title>Make-a-Ton</title>
-            <meta name="author" content="Sunith VS"/>
-            <meta charSet="utf-8"/>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-            <meta name="description" content="24 hour Hackathon"/>
-            <Head/>
-            <body>
-            <Main/>
-            <NextScript/>
-            <Analytics />
-            </body>
-        </Html>
-    )
+  return (
+    <Html lang="en">
+      <link rel="stylesheet" />
+      <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
+      <title>Make-A-Ton</title>
+      <meta name="author" content="Sunith VS" />
+      <meta charSet="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="description" content="24 hour Hackathon" />
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <Analytics />
+      </body>
+    </Html>
+  );
 }
