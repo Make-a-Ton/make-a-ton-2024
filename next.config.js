04a8f7733452ca18960c8dfa83250649b2432d1b
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-09-14 22:35:29 +0530

    config reset

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: "",
-  trailingSlash: false,
-  async rewrites() {
-    return [
-      {
-        source: "/:slug",
-        destination: "/social/:slug",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
