3d55ce3c16fec48fd2b7b604704948e050d7f5a8
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-09-15 12:20:50 +0530

    conductor track responsiveness

--- src/pages/conductor-track/index.jsx
+++ src/pages/conductor-track/index.jsx
@@ -4,166 +4,154 @@ import Image from "next/image";
 
 export default function ConductorTrack() {
   return (
-    <>
-      <div className="min-h-screen bg-[#204289] p-4 md:p-8 relative">
-        <h1 className="text-3xl md:text-4xl text-[#EAAE20] mb-8 text-center relative z-10 font-clash-bold">
+    <div className="min-h-screen bg-[#204289] p-4 md:p-8 relative">
+      <div className="flex flex-row items-center justify-center mb-8">
+        <Link
+          href="/"
+          className="fixed top-4 left-4 flex flex-row text-customYellow font-satoshi-regular gap-2 items-center back-button"
+        >
+          <Image
+            src="/assets/icons/pixelArrow.svg"
+            alt=""
+            width={50}
+            height={50}
+            className="rotate-180 w-8 h-8 md:w-12 md:h-12"
+          />
+          <span className="hidden md:block text-lg">Home</span>
+        </Link>
+        <h1 className="text-2xl md:text-4xl text-[#EAAE20] text-center relative z-10 font-clash-bold">
           Conductor Track
         </h1>
+      </div>
 
-        {/* Mobile View Image */}
-        <div className="block md:hidden text-center mb-8">
-          <div className="relative w-[80px] mx-auto h-auto">
+      {/* Mobile View Image */}
+      <div className="block md:hidden text-center mb-8">
+        <div className="relative w-20 mx-auto h-auto">
+          <Image
+            src="/assets/lines.png"
+            alt="Top Lines"
+            layout="responsive"
+            width={80}
+            height={200}
+            objectFit="contain"
+            quality={100}
+            priority
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-between gap-8 mx-auto max-w-7xl">
+        {/* Left Side */}
+        <div className="flex flex-col text-center md:text-left md:w-1/3 space-y-8">
+          <div>
+            <p className="font-clash-bold text-xl md:text-3xl text-white mb-2">
+              What is the Conductor Track?
+            </p>
+            <p className="font-satoshi-regular text-sm md:text-base text-white">
+              The Conductor Track is a special track sponsored by Orkes for
+              Make-A-Ton. This track offers exclusive prizes and provides
+              dedicated support and mentorship. To participate, it&apos;s
+              mandatory to register through Star-A-Ton and star the Orkes
+              Conductor GitHub repository.
+            </p>
+          </div>
+          <div className="md:ml-12">
+            <p className="font-clash-bold text-xl md:text-3xl text-white mb-2">
+              What is Orkes Conductor?
+            </p>
+            <p className="font-satoshi-regular text-sm md:text-base text-white md:px-4">
+              Orkes Conductor is a tool that helps you organize and automate
+              tasks in a sequence. Think of it as an automatic to-do list that
+              ensures everything happens in the right order.
+            </p>
+          </div>
+        </div>
+
+        {/* Center: Image */}
+        <div className="hidden md:flex flex-col items-center md:w-1/4">
+          <div className="relative w-20 h-auto">
             <Image
               src="/assets/lines.png"
               alt="Top Lines"
+              layout="responsive"
               width={80}
-              height={0}
-              style={{ height: "100%" }}
-              objectFit="cover"
+              height={400}
+              objectFit="contain"
               quality={100}
               priority
             />
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row items-center justify-center mb-8 mx-4 md:mx-8">
-          {/* Left Side: What is the Conductor Track and What is Orkes */}
-          <div className="flex flex-col text-left md:w-1/3 md:mr-8 md:-mt-16 w-full">
-            {/* Conductor Track Text */}
-            <div className="mb-6 text-center md:text-left">
-              <p className="font-clash-bold text-lg md:text-3xl text-white mb-2 w-full md:w-[560px]">
-                What is the Conductor Track?
-              </p>
-              <p className="font-satoshi-regular text-base text-white text-[13px]">
-                The Conductor Track is a special track sponsored by Orkes for
-                Make-A-Ton. This track offers exclusive prizes and provides
-                dedicated support and mentorship. To participate, its mandatory
-                to register through Star-A-Ton and star the Orkes Conductor
-                GitHub repository.
-              </p>
-            </div>
-            {/* Orkes Conductor Text */}
-            <div className="mb-4 md:ml-12 w-full md:w-[480px] text-center md:text-left">
-              <p className="font-clash-bold text-lg md:text-3xl text-white mb-2">
-                What is Orkes Conductor?
-              </p>
-              <p className="font-satoshi-regular text-base text-[13px] text-white md:px-12">
-                Orkes Conductor is a tool that helps you organize and automate
-                tasks in a sequence. Think of it as an automatic to-do list that
-                ensures everything happens in the right order.
-              </p>
-            </div>
-          </div>
-
-          {/* Center: Image */}
-          <div className="hidden md:flex flex-col items-center md:w-1/3">
-            <div className="relative w-full md:w-[80px] h-auto">
+        {/* Right Side */}
+        <div className="flex flex-col md:w-1/3 text-white  text-center">
+          {/* <div> */}
+          <h2 className="font-clash-bold text-xl md:text-4xl text-customYellow md:mb-4">
+            How to participate
+          </h2>
+          <ul className="font-satoshi-regular text-base items-center mt-2 md:text-xl flex flex-col gap-4">
+            <li className="flex items-center gap-2">
               <Image
-                src="/assets/lines.png"
-                alt="Top Lines"
-                width={80}
-                height={0}
-                style={{ height: "100%" }}
-                objectFit="cover"
-                quality={100}
-                priority
+                src="/assets/icons/githubStar.png"
+                alt="github star"
+                width={30}
+                height={27}
+                className="w-6 h-6 md:w-8 md:h-8"
               />
-            </div>
-          </div>
-
-          {/* Right Side: Exclusive Prizes and Register */}
-          <div className="flex flex-col md:w-[618px] md:ml-4 text-center md:-mt-16 mb-8 md:mb-0">
-            <div className="mb-12">
-              <h2 className="font-clash-bold text-xl md:text-3xl text-white">
-                Exclusive Prizes
-              </h2>
-              <h2 className="font-clash-bold text-xl md:text-3xl text-white">
-                and Swags
-              </h2>
-            </div>
-            <div className="md:pl-8 inline-flex justify-center items-center space-x-2">
+              <span>
+                Apply for{" "}
+                <Link
+                  href="/star-a-ton"
+                  className="underline text-customYellow"
+                >
+                  Star a ton
+                </Link>
+              </span>
+            </li>
+            <li className="flex items-center gap-2">
               <Image
                 src="/assets/hammer.png"
                 alt="hammer"
-                width={54}
-                height={50}
-                objectFit="cover"
-                quality={100}
+                width={27}
+                height={30}
+                className="w-6 h-6 md:w-8 md:h-8"
               />
-              <span className="font-clash-bold text-md md:text-3xl text-white">
-                Register{" "}
+              <span>
+                Register for Make-A-Ton 7.0{" "}
+                <a
+                  href="https://lu.ma/j58nctcm"
+                  className="text-customYellow underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  here
+                </a>
               </span>
-              <Link href="/star-a-ton" passHref>
-                <span className="font-clash-bold text-md md:text-3xl text-[#EAAE20] underline cursor-pointer inline-flex items-center">
-                  Here
-                </span>
-              </Link>
-            </div>
-          </div>
+            </li>
+            <li className="mt-2">
+              That&apos;s it, you are now considered for conductor track and
+              giveaway!
+            </li>
+          </ul>
+          {/* </div> */}
         </div>
-
-        {/* Yellow Ribbon for Desktop View */}
-        <div className="hidden md:block absolute bottom-0 left-0 mb-4 ml-4">
-          <div className="relative w-[120px] h-auto">
-            <Image
-              src="/assets/yellowribbon.png"
-              alt="Yellow Ribbon"
-              width={120}
-              height={0}
-              style={{ height: "auto" }}
-              objectFit="cover"
-              quality={100}
-              priority
-            />
-          </div>
-        </div>
-
-        {/* Mobile View Adjustments */}
-        <style jsx global>{`
-          @media (max-width: 768px) {
-            .flex-col {
-              align-items: center;
-            }
-            .text-left {
-              text-align: center;
-            }
-            .w-[560px],
-            .w-[480px] {
-              width: 100%;
-            }
-            .ml-12,
-            .px-12 {
-              margin-left: 0;
-              padding-left: 1rem;
-              padding-right: 1rem;
-            }
-            .md\:w-1/3 {
-              width: 100%;
-            }
-            .md\:mr-8,
-            .md\:ml-4 {
-              margin-right: 0;
-              margin-left: 0;
-            }
-            .mx-4 {
-              margin-left: 2rem;
-              margin-right: 2rem;
-            }
-            .text-center {
-              text-align: center;
-            }
-            .mb-8 {
-              margin-bottom: 2rem;
-            }
-            .mb-6 {
-              margin-bottom: 1.5rem;
-            }
-            .mb-4 {
-              margin-bottom: 1rem;
-            }
-          }
-        `}</style>
       </div>
-    </>
+
+      {/* Yellow Ribbon for Desktop View */}
+      <div className="hidden md:block absolute bottom-4 left-4">
+        <div className="relative w-24 h-auto">
+          <Image
+            src="/assets/yellowribbon.png"
+            alt="Yellow Ribbon"
+            layout="responsive"
+            width={120}
+            height={160}
+            objectFit="contain"
+            quality={100}
+            priority
+          />
+        </div>
+      </div>
+    </div>
   );
 }
