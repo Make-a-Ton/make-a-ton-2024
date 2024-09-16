bc2b54ca7880165f6ff2699b9a40d98a36ac5486
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-09-16 09:32:28 +0530

    added ca route

---
+++ src/pages/ca/index.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+export default function X() {
+  useEffect(() => {
+    window.location.assign("https://forms.gle/Eu9HpCNGwsd7v58X7");
+  });
+  return <></>;
+}
