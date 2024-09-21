7cbcf943dc326a61cfa39b7fec7e2f50a08ca1f5
AmeenKM27 <amsn27km@gmail.com>
2024-09-21 13:49:34 +0530

    added cfm

--- src/pages/cfm/index.js
+++ src/pages/cfm/index.js
@@ -3,7 +3,7 @@ const index = () => {
     <div className="h-screen ">
       <iframe
         class="airtable-embed"
-        src="https://airtable.com/appbx3uK70QNE5GL3/pagebfc0YgJOoungT/form"
+        src="https://airtable.com/embed/appbx3uK70QNE5GL3/pagebfc0YgJOoungT/form"
         frameborder="0"
         onmousewheel=""
         width="100%"
--- src/pages/design-a-ton/index.jsx
+++ src/pages/design-a-ton/index.jsx
@@ -3,7 +3,7 @@ const index = () => {
     <div className="h-screen ">
       <iframe
         class="airtable-embed"
-        src="https://airtable.com/appbx3uK70QNE5GL3/pagLY2tgWZg0hsXJQ/form"
+        src="https://airtable.com/embed/appbx3uK70QNE5GL3/pagLY2tgWZg0hsXJQ/form"
         frameborder="0"
         onmousewheel=""
         width="100%"
